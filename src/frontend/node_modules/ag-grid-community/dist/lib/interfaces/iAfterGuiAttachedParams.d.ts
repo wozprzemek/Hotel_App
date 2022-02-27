8fa2f1c52067a22ef31248cfefc24243cd95f20f
wozprzemek <37674730+wozprzemek@users.noreply.github.com>
2022-02-27 22:21:33 +0100

    Delete src/frontend/node_modules directory

--- src/frontend/node_modules/ag-grid-community/dist/lib/interfaces/iAfterGuiAttachedParams.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type ContainerType = 'columnMenu' | 'contextMenu' | 'toolPanel' | 'floatingFilter';
-export interface IAfterGuiAttachedParams {
-    container?: ContainerType;
-    hidePopup?: () => void;
-    suppressFocus?: boolean;
-}
