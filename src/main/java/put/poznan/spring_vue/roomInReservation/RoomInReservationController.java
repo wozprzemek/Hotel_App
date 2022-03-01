e4fd9f5e58d5c05c6cecb86b33b5d8a72b659abc
Leskyyy <leskyyyy@gmail.com>
2022-03-01 20:56:57 +0100

    added RoomInReservation handling

--- src/main/java/put/poznan/spring_vue/roomInReservation/RoomInReservationController.java
+++ src/main/java/put/poznan/spring_vue/roomInReservation/RoomInReservationController.java
@@ -1,27 +1,48 @@
-//package put.poznan.spring_vue.roomInReservation;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import put.poznan.spring_vue.guest.Guest;
-//import put.poznan.spring_vue.hotel.Hotel;
-//import put.poznan.spring_vue.reservation.Reservation;
-//import put.poznan.spring_vue.room.Room;
-//
-//@RestController // This means that this class is a Controller
-//@RequestMapping(path="/api/roominrsv") // This means URL's start with /demo (after Application path)
-//public class RoomInReservationController {
-//    @Autowired
-//    private RoomInReservationRepository roomInReservationRepository;
-//
-//    @PostMapping(path="/add") // Map ONLY POST Requests
-//    public ResponseEntity<RoomInReservation> addNewRoomInReservation (@RequestBody RoomInReservationDetails roomInReservationDetails) {
-//        try{
-//
-//            return new ResponseEntity<>(_roomInReservation, HttpStatus.CREATED);
-//        } catch (Exception e){
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
-//}
+package put.poznan.spring_vue.roomInReservation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import put.poznan.spring_vue.guest.Guest;
+import put.poznan.spring_vue.hotel.Hotel;
+import put.poznan.spring_vue.reservation.Reservation;
+import put.poznan.spring_vue.room.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController // This means that this class is a Controller
+@RequestMapping(path="/api/roominrsv") // This means URL's start with /demo (after Application path)
+public class RoomInReservationController {
+    @Autowired
+    private RoomInReservationRepository roomInReservationRepository;
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public ResponseEntity<List<RoomInReservation>> addNewRoomInReservation (@RequestBody RoomInReservationDetails roomInReservationDetails) {
+        try{
+            int reservationID = roomInReservationDetails.getReservationID();
+            Reservation reservation = roomInReservationRepository.findReservationByID(reservationID);
+            Hotel hotel = reservation.getHotel();
+            Guest guest = reservation.getGuest();
+            List<Integer> rooms = roomInReservationDetails.getRooms();
+
+            RoomInReservation roomInReservation;
+            List<RoomInReservation> toReturn = new ArrayList<>();
+
+            for(int i=0; i<rooms.size(); i++){
+                Room roomToAdd = roomInReservationRepository.findRoomByNumber(rooms.get(i));
+                roomInReservation = new RoomInReservation(0, roomToAdd, hotel, reservation, guest);
+                RoomInReservation _roomInReservation = roomInReservationRepository.save(roomInReservation);
+                roomInReservationRepository.setFullRoomPrice(reservation.getStartDate(), reservation.getEndDate(), rooms.get(i), _roomInReservation.getId());
+                roomInReservation.setRoomTotalPrice(roomInReservationRepository.findById(_roomInReservation.getId()).getRoomTotalPrice());
+                toReturn.add(roomInReservation);
+            }
+
+            return new ResponseEntity<>(toReturn, HttpStatus.CREATED);
+        } catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- src/main/java/put/poznan/spring_vue/roomInReservation/RoomInReservationDetails.java
+++ src/main/java/put/poznan/spring_vue/roomInReservation/RoomInReservationDetails.java
@@ -1,10 +1,25 @@
 package put.poznan.spring_vue.roomInReservation;
 
+import java.util.List;
+
 public class RoomInReservationDetails {
 
-    private float totalRoomPrice;
-    private int hotelID;
     private int reservationID;
-    private int guestID;
+    private List<Integer> rooms;
 
+    public int getReservationID() {
+        return reservationID;
+    }
+
+    public void setReservationID(int reservationID) {
+        this.reservationID = reservationID;
+    }
+
+    public List<Integer> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Integer> rooms) {
+        this.rooms = rooms;
+    }
 }
