d08368d4e35e63843a365d06386e53fab136fc39
Leskyyy <leskyyyy@gmail.com>
2022-03-06 18:52:57 +0100

    added adding cities

--- src/main/java/put/poznan/spring_vue/city/CityRepository.java
+++ src/main/java/put/poznan/spring_vue/city/CityRepository.java
@@ -1,11 +1,19 @@
 package put.poznan.spring_vue.city;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import put.poznan.spring_vue.admin.Admin;
+import put.poznan.spring_vue.country.Country;
 
 import java.util.List;
 
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    @Query("SELECT ct FROM Country ct WHERE ct.countryName = :countryName")
+    Country findCountryByCountryName(@Param("countryName") String countryName);
+
+    City findCityByCityName(String cityName);
 
 }
